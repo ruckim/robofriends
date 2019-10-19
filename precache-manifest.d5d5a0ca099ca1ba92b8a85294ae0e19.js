373d9c55cc61739e8d8376ae2103867f3a8d0a94
ruckim <marthy94@gmail.com>
2019-10-19 15:27:47 +0200

    Updates

---
+++ precache-manifest.d5d5a0ca099ca1ba92b8a85294ae0e19.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81b0b40af257db7f4ebc026125d077d0",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "7d96eb48e92577131826",
+    "url": "/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "2245795d2339032fa86c",
+    "url": "/robofriends/static/css/main.18e71004.chunk.css"
+  },
+  {
+    "revision": "7d96eb48e92577131826",
+    "url": "/robofriends/static/js/2.29b9d72c.chunk.js"
+  },
+  {
+    "revision": "2245795d2339032fa86c",
+    "url": "/robofriends/static/js/main.01ddd1ca.chunk.js"
+  },
+  {
+    "revision": "15646e7ae3f7e7d4e926",
+    "url": "/robofriends/static/js/runtime-main.59eca5ac.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
